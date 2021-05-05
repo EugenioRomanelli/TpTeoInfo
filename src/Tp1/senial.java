7afaab2cbd99dad013eff569a538a19841156733
Yamil <yamilchaju@gmail.com>
2021-05-05 11:08:08 -0300

    prueba

--- src/Tp1/senial.java
+++ src/Tp1/senial.java
@@ -1,10 +1,20 @@
 package Tp1;
 
+import java.io.File;
+import java.io.IOException;
+
 class senial {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws IOException {
+		
+		File ETH = new File("ETH");
+		File BTC = new File("BTC");
+		if (ETH.exists() && BTC.exists()) {
+			System.out.print("hola");
+		}
+		}
 
 	}
+	
 
-}
+
